9003c7aee0e897482066481d2686d4440d711f3e
Chester <chester.htooaunglinn@gmail.com>
2023-07-19 16:52:28 +0100

    feat: update output values from modules

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,7 +3,6 @@ output "s3" {
 
   value = {
     bucket_id  = module.s3_bucket.s3_bucket_id
-    bucket_arn = module.s3_bucket.s3_bucket_arn
   }
 }
 
@@ -14,7 +13,5 @@ output "cloudfront" {
   value = {
     distribution_id = module.cloudfront.cloudfront_distribution_id
     domain          = module.cloudfront.cloudfront_distribution_domain_name
-    arn             = module.cloudfront.cloudfront_distribution_arn
-    oai_ids         = module.cloudfront.cloudfront_origin_access_identities
   }
 }
